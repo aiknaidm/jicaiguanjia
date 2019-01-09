44339ae1e76073d333a0c3e4d3baca045a2f04a8
salad1987 <qianxunde1987@163.com>
2019-01-09 14:59:06 +0800

    客服管理

--- API/api.js
+++ API/api.js
@@ -1,20 +1,23 @@
 const host = "https://lmbge.com/wxapi/jcgj"
 const host_test = "https://lmbge.com/wxapi"
 const api = {
-    login: `${host}/denglu`, //登录
-    getUserInfo: `${host}/get_user_info`, //用户信息
-    upUserInfo: `${host}/up_user_info`,
-    getYizhanInfo: `${host}/jlxx`,
-    yzm: `${host}/yzm`,
-    renzheng: `${host}/renzheng`,
-    message: `${host}/zixun`, //资讯
-    carousel: `${host}/lunbo`, //轮播图
-    yizhanIndex: `${host}/wdyz`, //商城首页
-    yizhanBanner: `${host}/yzgg`, //首页广告
-    getShareImg: `${host}/getQRCode2`,
-    fxfxList: `${host}/fxfx_list`,
-    fxfxDetail: `${host}/fxfx_detail`,
-    jlList: `${host}/jl_list`,
-    orderList: `${host}/ddlb`
+  login: `${host}/denglu`, //登录
+  getUserInfo: `${host}/get_user_info`, //用户信息
+  upUserInfo: `${host}/up_user_info`,
+  getYizhanInfo: `${host}/jlxx`,
+  yzm: `${host}/yzm`,
+  renzheng: `${host}/renzheng`,
+  message: `${host}/zixun`, //资讯
+  carousel: `${host}/lunbo`, //轮播图
+  yizhanIndex: `${host}/wdyz`, //商城首页
+  yizhanBanner: `${host}/yzgg`, //首页广告
+  getShareImg: `${host}/getQRCode2`,
+  fxfxList: `${host}/fxfx_list`,
+  fxfxDetail: `${host}/fxfx_detail`,
+  jlList: `${host}/jl_list`,
+  orderList: `${host}/ddlb`,
+  serviceList: `${host}/kefu_list`,
+  addService: `${host}/kefu_add`,
+  delService: `${host}/kefu_del`
 }
 export default api
