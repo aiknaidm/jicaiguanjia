5159698563ca14067f8420c2a6ffe1eb7c62e7e8
aiknaidm <aiknaidm@163.com>
2019-01-15 14:50:52 +0800

    交易管理

--- API/api.js
+++ API/api.js
@@ -1,44 +1,59 @@
 const host = "https://lmbge.com/wxapi/jcgj"
 const host_test = "https://lmbge.com/wxapi"
 const api = {
-  login: `${host}/denglu`, //登录
-  getUserInfo: `${host}/get_user_info`, //用户信息
-  upUserInfo: `${host}/up_user_info`,
-  getYizhanInfo: `${host}/jlxx`,
-  yzm: `${host}/yzm`,
-  renzheng: `${host}/renzheng`,
-  message: `${host}/zixun`, //资讯
-  carousel: `${host}/lunbo`, //轮播图
-  yizhanIndex: `${host}/wdyz`, //商城首页
-  yizhanBanner: `${host}/yzgg`, //首页广告
-  getShareImg: `${host}/getQRCode2`,
-  fxfxList: `${host}/fxfx_list`,
-  fxfxDetail: `${host}/fxfx_detail`,
-  jlList: `${host}/jl_list`,
-  orderList: `${host}/ddlb`,
-  serviceList: `${host}/kefu_list`,
-  addService: `${host}/kefu_add`,
-  delService: `${host}/kefu_del`,
-  yhlb: `${host}/yhlb`,
-  jcgx: `${host}/jcgx`,
-  yhsh: `${host}/yhsh`,
-  find_users: `${host}/find_users`,
-  find_users_c: `${host}/find_users_c`,
-  addMember: `${host}/tjhy`,
-  addManager: `${host}/manager_add`,
-  delManager: `${host}/manager_del`,
-  yhxq: `${host}/yhxq`,
-  findManagers: `${host}/find_managers`,
-  verifyCode: `${host}/chk_share_code`,
-  renewList: `${host}/fuwufei`,
-  fwfwxpay: `${host}/fwfwxpay`,
-  fwfpaysuccess: `${host}/fwfpaysuccess`,
-  fwfpaylist: `${host}/fwfpaylist`,
-  supplier_upd: `${host}/supplier_upd`,
-  fwfwxpay: `${host}/fwfwxpay`,
-  fwfpaysuccess: `${host}/fwfpaysuccess`,
-  findSalesman: `${host}/findSalesman`,
-  delSalesman: `${host}/delSalesman`,
-  addSalesman: `${host}/addSalesman`
+    login: `${host}/denglu`, //登录
+    getUserInfo: `${host}/get_user_info`, //用户信息
+    upUserInfo: `${host}/up_user_info`,
+    getYizhanInfo: `${host}/jlxx`,
+    yzm: `${host}/yzm`,
+    renzheng: `${host}/renzheng`,
+    message: `${host}/zixun`, //资讯
+    carousel: `${host}/lunbo`, //轮播图
+    yizhanIndex: `${host}/wdyz`, //商城首页
+    yizhanBanner: `${host}/yzgg`, //首页广告
+    getShareImg: `${host}/getQRCode2`,
+    fxfxList: `${host}/fxfx_list`,
+    fxfxDetail: `${host}/fxfx_detail`,
+    jlList: `${host}/jl_list`,
+    orderList: `${host}/ddlb`,
+    orderDetail: `${host}/ddxq`,
+    serviceList: `${host}/kefu_list`,
+    addService: `${host}/kefu_add`,
+    delService: `${host}/kefu_del`,
+    yhlb: `${host}/yhlb`,
+    jcgx: `${host}/jcgx`,
+    yhsh: `${host}/yhsh`,
+    find_users: `${host}/find_users`,
+    find_users_c: `${host}/find_users_c`,
+    addMember: `${host}/tjhy`,
+    addManager: `${host}/manager_add`,
+    delManager: `${host}/manager_del`,
+    yhxq: `${host}/yhxq`,
+    findManagers: `${host}/find_managers`,
+    verifyCode: `${host}/chk_share_code`,
+    renewList: `${host}/fuwufei`,
+    fwfwxpay: `${host}/fwfwxpay`,
+    fwfpaysuccess: `${host}/fwfpaysuccess`,
+    fwfpaylist: `${host}/fwfpaylist`,
+    supplier_upd: `${host}/supplier_upd`,
+    fwfwxpay: `${host}/fwfwxpay`,
+    fwfpaysuccess: `${host}/fwfpaysuccess`,
+    findSalesman: `${host}/findSalesman`,
+    delSalesman: `${host}/delSalesman`,
+    addSalesman: `${host}/addSalesman`,
+    add_categorys: `${host}/add_categorys`, //添加分类
+    del_categorys: `${host}/del_categorys`, //删除分类
+    del_brands: `${host}/del_brands`, //删除品牌
+    brands: `${host}/brands`,
+    upload: `${host}/upload`, //上传图片
+    categorys: `${host}/categorys`, //分类列表
+    addGoodsInfo: `${host}/add_goods`,
+    search: `${host}/search`, // https://lmbge.com/wxapi/gztai/search
+    allcats: `${host}/allcats`,
+    goodinfo: `${host}/goodinfo`,
+    changePrice: `${host}/xgjg`, //修改价格
+    changeNum: `${host}/xgkc`, //修改价格
+    fahuodan: `${host}/fahuodan`,
+    fahuo: `${host}/fahuo`
 }
 export default api
--- utils/index.js
+++ utils/index.js
@@ -1,226 +1,253 @@
 import wepy from 'wepy'
-const formatTime = function (time, fmt) {
-  var o = {
-    "M+": time.getMonth() + 1, //月份 
-    "d+": time.getDate(), //日 
-    "h+": time.getHours(), //小时 
-    "m+": time.getMinutes(), //分 
-    "s+": time.getSeconds(), //秒 
-    "q+": Math.floor((time.getMonth() + 3) / 3), //季度 
-    "S": time.getMilliseconds() //毫秒 
-  };
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (time.getFullYear() + "").substr(4 - RegExp.$1.length));
-  }
-  for (var k in o) {
-    if (new RegExp("(" + k + ")").test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+const formatTime = function(time, fmt) {
+        var o = {
+            "M+": time.getMonth() + 1, //月份 
+            "d+": time.getDate(), //日 
+            "h+": time.getHours(), //小时 
+            "m+": time.getMinutes(), //分 
+            "s+": time.getSeconds(), //秒 
+            "q+": Math.floor((time.getMonth() + 3) / 3), //季度 
+            "S": time.getMilliseconds() //毫秒 
+        };
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (time.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+        for (var k in o) {
+            if (new RegExp("(" + k + ")").test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            }
+        }
+        return fmt;
     }
-  }
-  return fmt;
-}
-// orderId 订单id redirectUrl 跳转url failUrl 失败跳转
-// 支付
-const wxpay1 = async function (orderId) {
-  //   let remark = "在线充值";
-  //   let nextAction = {};
-  //   if (orderId != 0) {
-  //    let remark = "支付订单 ：" + orderId;
-  //    let nextAction = {
-  //       type: 0,
-  //       id: orderId
-  //     };
-  //   }
-  var res0 = await wepy.login()
-  var code = res0.code
-  if (!code) {
-    return {
-      code: 0,
-      msg: "登录失败"
-    };
-  }
-  var res = await wepy.request({
-    url: 'https://lmbge.com/wxapi/jicai/wxpay1',
-    data: {
-      weixin: res0.code,
-      id: orderId
-    },
-  })
-  var result = res.data.data;
-  console.log(res.data)
-  if (res.data.code == 0) {
-    //  通知用
-    var prepay_id = result.package.replace("prepay_id=", "");
-    // 发起支付
+    // orderId 订单id redirectUrl 跳转url failUrl 失败跳转
+    // 支付
+const wxpay1 = async function(orderId) {
+        //   let remark = "在线充值";
+        //   let nextAction = {};
+        //   if (orderId != 0) {
+        //    let remark = "支付订单 ：" + orderId;
+        //    let nextAction = {
+        //       type: 0,
+        //       id: orderId
+        //     };
+        //   }
+        var res0 = await wepy.login()
+        var code = res0.code
+        if (!code) {
+            return {
+                code: 0,
+                msg: "登录失败"
+            };
+        }
+        var res = await wepy.request({
+            url: 'https://lmbge.com/wxapi/jicai/wxpay1',
+            data: {
+                weixin: res0.code,
+                id: orderId
+            },
+        })
+        var result = res.data.data;
+        console.log(res.data)
+        if (res.data.code == 0) {
+            //  通知用
+            var prepay_id = result.package.replace("prepay_id=", "");
+            // 发起支付
 
+            try {
+                var payres = await wepy.requestPayment({
+                    timeStamp: result.timeStamp,
+                    nonceStr: result.nonceStr,
+                    package: result.package,
+                    signType: result.signType,
+                    paySign: result.paySign,
+
+                })
+                wepy.request({
+                    url: 'https://lmbge.com/wxapi/jicai/paysuccess',
+                    data: {
+                        id: orderId
+                    },
+                })
+
+                // wx.redirectTo({
+                //     url: redirectUrl
+                // });
+                return {
+
+                    code: 1,
+                    msg: "支付成功"
+                };
+
+            } catch (err) {
+                // 取消支付 
+                console.log("取消支付")
+                return {
+                    code: 2,
+                    msg: "取消支付"
+                };
+
+            }
+        } else {
+            return {
+                code: 3,
+                msg: "服务器忙"
+            };
+        }
+    }
+    // 提现到零钱
+const wxpay3 = async function(user_id) {
+
+    var res0 = await wepy.login()
+    var code = res0.code
+    if (!code) {
+        return {
+            code: 0,
+            msg: "登录失败"
+        };
+    }
     try {
-      var payres = await wepy.requestPayment({
-        timeStamp: result.timeStamp,
-        nonceStr: result.nonceStr,
-        package: result.package,
-        signType: result.signType,
-        paySign: result.paySign,
+        var res = await wepy.request({
+            url: 'https://lmbge.com/wxapi/jicai/wxpay3',
+            data: {
+                weixin: res0.code,
+                user_id,
+                desc: "提现"
+            },
+        })
+        var result = res.data.data;
+        console.log(res.data)
+        if (res.data.code == 0) {
 
-      })
-      wepy.request({
-        url: 'https://lmbge.com/wxapi/jicai/paysuccess',
-        data: {
-          id: orderId
-        },
-      })
+            return {
 
-      // wx.redirectTo({
-      //     url: redirectUrl
-      // });
-      return {
+                code: 0,
+                msg: "提现成功"
+            };
 
-        code: 1,
-        msg: "支付成功"
-      };
 
-    } catch (err) {
-      // 取消支付 
-      console.log("取消支付")
-      return {
-        code: 2,
-        msg: "取消支付"
-      };
+        } else {
+            return {
+                code: 1,
+                msg: "提现失败"
+            };
+        }
+    } catch (error) {
+        return {
+
+            code: 2,
+            msg: "提现失败"
+        };
 
     }
-  } else {
-    return {
-      code: 3,
-      msg: "服务器忙"
-    };
-  }
-}
-// 提现到零钱
-const wxpay3 = async function (user_id) {
-
-  var res0 = await wepy.login()
-  var code = res0.code
-  if (!code) {
-    return {
-      code: 0,
-      msg: "登录失败"
-    };
-  }
-  try {
-    var res = await wepy.request({
-      url: 'https://lmbge.com/wxapi/jicai/wxpay3',
-      data: {
-        weixin: res0.code,
-        user_id,
-        desc: "提现"
-      },
-    })
-    var result = res.data.data;
-    console.log(res.data)
-    if (res.data.code == 0) {
-
-      return {
-
-        code: 0,
-        msg: "提现成功"
-      };
-
-
-    } else {
-      return {
-        code: 1,
-        msg: "提现失败"
-      };
-    }
-  } catch (error) {
-    return {
-
-      code: 2,
-      msg: "提现失败"
-    };
-
-  }
 }
 
 // 服务费缴费
-const fwfwxpay = async function (user_id, suppliers_id, data, pid) {
-  var res0 = await wepy.login()
-  var code = res0.code
-  if (!code) {
-    return {
-      code: 0,
-      msg: "登录失败"
-    };
-  }
-  var res = await wepy.request({
-    url: 'https://lmbge.com/wxapi/index/fwfwxpay',
-    data: {
-      user_id: user_id,
-      suppliers_id: suppliers_id,
-      weixin: res0.code,
-      goods_id: data.id,
-      goods_name: data.goods_name,
-      goods_price: data.isHasShareCode == "0" ? data.goods_price : data.youhui_price,
-      months: data.months
-    },
-  })
-  var result = res.data.data;
-  console.log(res.data)
-  if (res.data.code == 0) {
-    //  通知用
-    var prepay_id = result.package.replace("prepay_id=", "");
-    // 发起支付
-    try {
-      var payres = await wepy.requestPayment({
-        timeStamp: result.timeStamp,
-        nonceStr: result.nonceStr,
-        package: result.package,
-        signType: result.signType,
-        paySign: result.paySign,
-      })
-      wepy.request({
-        url: 'https://lmbge.com/wxapi/index/fwfpaysuccess',
-        data: {
-          id: payres.data.orderId,
-          months: data.months,
-          user_id: user_id,
-          suppliers_id: suppliers_id,
-          share_code: pid
-        },
-      })
-      return {
-        code: 1,
-        msg: "支付成功"
-      };
+const fwfwxpay = async function(user_id, suppliers_id, data, pid) {
+        var res0 = await wepy.login()
+        var code = res0.code
+        if (!code) {
+            return {
+                code: 0,
+                msg: "登录失败"
+            };
+        }
+        var res = await wepy.request({
+            url: 'https://lmbge.com/wxapi/index/fwfwxpay',
+            data: {
+                user_id: user_id,
+                suppliers_id: suppliers_id,
+                weixin: res0.code,
+                goods_id: data.id,
+                goods_name: data.goods_name,
+                goods_price: data.isHasShareCode == "0" ? data.goods_price : data.youhui_price,
+                months: data.months
+            },
+        })
+        var result = res.data.data;
+        console.log(res.data)
+        if (res.data.code == 0) {
+            //  通知用
+            var prepay_id = result.package.replace("prepay_id=", "");
+            // 发起支付
+            try {
+                var payres = await wepy.requestPayment({
+                    timeStamp: result.timeStamp,
+                    nonceStr: result.nonceStr,
+                    package: result.package,
+                    signType: result.signType,
+                    paySign: result.paySign,
+                })
+                wepy.request({
+                    url: 'https://lmbge.com/wxapi/index/fwfpaysuccess',
+                    data: {
+                        id: payres.data.orderId,
+                        months: data.months,
+                        user_id: user_id,
+                        suppliers_id: suppliers_id,
+                        share_code: pid
+                    },
+                })
+                return {
+                    code: 1,
+                    msg: "支付成功"
+                };
 
-    } catch (err) {
-      // 取消支付 
-      console.log("取消支付")
-      return {
-        code: 2,
-        msg: "取消支付"
-      };
+            } catch (err) {
+                // 取消支付 
+                console.log("取消支付")
+                return {
+                    code: 2,
+                    msg: "取消支付"
+                };
 
+            }
+        } else {
+            return {
+                code: 3,
+                msg: "服务器忙"
+            };
+        }
     }
-  } else {
-    return {
-      code: 3,
-      msg: "服务器忙"
-    };
-  }
-}
+    // 收集formid
+const submitFormId = function(userId, formId) {
+    if (formId == "the formId is a mock one") {
+        return
+    }
+    wx.request({
+        url: 'https://lmbge.com/wxapi/index/add_form',
+        data: {
+            user_id: userId,
+            form_id: formId,
 
-//邮箱以及手机的正则表达式
-const regexConfig = function () {
-  var reg = {
-    email: /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/,
-    phone: /^1(3|4|5|7|8)\d{9}$/
-  }
-  return reg;
+        }
+    })
+}
+const html_decode = function(str) {
+        var s = "";
+        if (str.length == 0) return "";
+        // s = str.replace(/&gt;/g, "&");
+        s = str.replace(/&lt;/g, "<");
+        s = s.replace(/&gt;/g, ">");
+        s = s.replace(/&nbsp;/g, " ");
+        s = s.replace(/&#39;/g, "\'");
+        s = s.replace(/&quot;/g, "\"");
+        s = s.replace(/<br>/g, "\n");
+        return s;
+    }
+    //邮箱以及手机的正则表达式
+const regexConfig = function() {
+    var reg = {
+        email: /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/,
+        phone: /^1(3|4|5|7|8)\d{9}$/
+    }
+    return reg;
 }
 export default {
-  formatTime,
-  wxpay1,
-  wxpay3,
-  fwfwxpay,
-  regexConfig
+    formatTime,
+    wxpay1,
+    wxpay3,
+    fwfwxpay,
+    regexConfig,
+    submitFormId,
+    html_decode
 }
