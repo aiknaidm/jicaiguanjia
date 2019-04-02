6b1550e4125d4d43606c05d0ce86f7587c8a7a6f
salad1987 <qianxunde1987@163.com>
2019-04-02 17:39:48 +0800

    剩余api

--- API/api.js
+++ API/api.js
@@ -1,16 +1,10 @@
 const host = "https://lmbge.com/wxapi/jcgj"
 const host_test = "https://lmbge.com/wxapi"
 const api = {
-  search: `${host}/search`,
   upload: `${host}/upload`, //上传图片
+  fahuodan: `${host}/fahuodan`,
   kthypay: `${host}/kthypay`,
   kthypaysuccess: `${host}/kthypaysuccess`,
-  find_users: `${host}/find_users`,
-  find_users_c: `${host}/find_users_c`,
-  addMember: `${host}/tjhy`,
-  addManager: `${host}/manager_add`,
-  addSalesman: `${host}/addSalesman`,
-  fahuodan: `${host}/fahuodan`,
 
 }
 export default api
