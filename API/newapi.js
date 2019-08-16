6e99524379bed9db2abcc7809cd341ca139f5b19
aiknaidm <aiknaidm@163.com>
2019-08-16 16:36:40 +0800

    收益分成

--- API/newapi.js
+++ API/newapi.js
@@ -163,7 +163,7 @@ const newapi = {
     suppliers_goods_list: (data) => fetch(`${host_chk}/suppliers_goods_list`, 'GET', data),
     baozhengjin_pay: (data) => fetch(`${host_pay}/baozhengjin_pay`, 'GET', data),
     baozhengjin_paysuccess: (data) => fetch(`${host_pay}/baozhengjin_paysuccess`, 'GET', data),
-    suppliers_order_list: (data) => fetch(`${host_chk}/suppliers_order_list`, 'GET', data),
+    suppliers_order_list: (data) => fetch(`${host_chk}/suppliers_order_list`, 'POST', data),
 
 }
 const fetch = async function(url, method, data) {
